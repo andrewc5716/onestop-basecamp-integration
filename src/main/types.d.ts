53ddc966367ae665ed94fb0a566e26d107c301cd
Josh Wong <34504936+jbwong05@users.noreply.github.com>
2024-08-29 12:34:25 -0700

    Onestop scanning implementation (#2)
    * Initial implementation of Onestop scanning
    
    Functional after an initial test accounting for hyperlinks and strikethrough but definitely needs to be cleaned up
    
    * Function refactoring and Date and strikethrough fixes
    
    Refactored a lot of the functions and added proper Date and strikethrough recognition support
    
    * Added function comments
    
    * Removed output param and updated event row checking

--- src/main/types.d.ts
+++ src/main/types.d.ts
@@ -4,3 +4,25 @@ declare interface OpenEvent {
   readonly triggerUid?: string,
   readonly user?: GoogleAppsScript.Base.User
 }
+
+type Spreadsheet = GoogleAppsScript.Spreadsheet.Spreadsheet;
+type Sheet = GoogleAppsScript.Spreadsheet.Sheet;
+
+declare interface Text {
+  readonly value: string,
+  readonly hyperlink: string | null
+}
+
+declare interface Row {
+  readonly startTime: Date,
+  readonly endTime: Date,
+  readonly who: string,
+  readonly numAttendees: number,
+  readonly what: Text,
+  readonly where: Text,
+  readonly inCharge: Text,
+  readonly helpers: Text,
+  readonly foodLead: Text,
+  readonly childcare: Text,
+  readonly notes: Text
+}
