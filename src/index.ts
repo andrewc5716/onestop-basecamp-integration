669af26b35c7ac6d94ebf467d62cb12fd004017c
Josh Wong <34504936+jbwong05@users.noreply.github.com>
2024-09-04 19:48:35 -0700

    Add row unique identifiers to metadata (#4)
    * Add row unique identifiers to metadata
    
    * Removed row and unique from function names and removed null return value
    
    * Renamed metadata to row
    
    * Add all properties to toString()
    
    * Renamed assignId() to generateIdForRow()

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import {onOpen} from './main/menu'
+import {onOpen} from './main/menu';
 
-export * from './main/basecamp'
-export * from './main/scan'
+export * from './main/basecamp';
+export * from './main/row';
+export * from './main/scan';
 
 // Re-export all the exports from `menu` module into Webpack `globalThis` scope, so they are available at runtime
 // for the Google Apps Script to call.
-export * from './main/menu'
+export * from './main/menu';
 
 // Assign `global` so gas-webpack-plugin will generate a stub for `onOpen` in the top-level scope. A top-level
 // declaration is needed for Google Apps Script to detect the function, so it is available as a trigger.
-global.onOpen = onOpen
+global.onOpen = onOpen;
