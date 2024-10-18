3f0c2e1382cb878739fb355c86584dbfa808391c
Josh Wong <jbwong05@gmail.com>
2024-10-18 16:47:00 -0700

    Fixed indentation

--- __test__/row.test.ts
+++ __test__/row.test.ts
@@ -7,110 +7,110 @@ import { getId } from "../src/main/row";
 import { RowMissingIdError } from '../src/main/error/rowMissingIdError';
 
 describe("getId", () => {
-  test("Row has an id", () => {
-    const getValueMock = jest.fn(() => "0419f086-4f19-4ade-ac64-2edafefef23d");
-    const metadataMock: Metadata = {
-      getId: jest.fn(),
-      getKey: jest.fn(),
-      getLocation: jest.fn(),
-      getValue: getValueMock,
-      getVisibility: jest.fn(),
-      moveToColumn: jest.fn().mockReturnThis(),
-      moveToRow: jest.fn().mockReturnThis(),
-      moveToSheet: jest.fn().mockReturnThis(),
-      moveToSpreadsheet: jest.fn().mockReturnThis(),
-      remove: jest.fn(),
-      setKey: jest.fn().mockReturnThis(),
-      setValue: jest.fn().mockReturnThis(),
-      setVisibility: jest.fn().mockReturnThis(),
-    };
-    const row: Row = {
-      metadata: metadataMock,
-      startTime: new Date("2024-10-18T07:00:00"),
-      endTime: new Date("2024-10-18T08:00:00"),
-      who: "Everyone",
-      numAttendees: 100,
-      what: {value: "DT + Prayer", hyperlink: null},
-      where: {value: "Viewridge", hyperlink: null},
-      inCharge: {value: "Person1", hyperlink: null},
-      helpers: {value: "Setup: Helper1, Helper2, Helper3", hyperlink: null},
-      foodLead: {value: "", hyperlink: null},
-      childcare: {value: "", hyperlink: null},
-      notes: {value: "", hyperlink: null}
-    };
+    test("Row has an id", () => {
+        const getValueMock = jest.fn(() => "0419f086-4f19-4ade-ac64-2edafefef23d");
+        const metadataMock: Metadata = {
+            getId: jest.fn(),
+            getKey: jest.fn(),
+            getLocation: jest.fn(),
+            getValue: getValueMock,
+            getVisibility: jest.fn(),
+            moveToColumn: jest.fn().mockReturnThis(),
+            moveToRow: jest.fn().mockReturnThis(),
+            moveToSheet: jest.fn().mockReturnThis(),
+            moveToSpreadsheet: jest.fn().mockReturnThis(),
+            remove: jest.fn(),
+            setKey: jest.fn().mockReturnThis(),
+            setValue: jest.fn().mockReturnThis(),
+            setVisibility: jest.fn().mockReturnThis(),
+        };
+        const row: Row = {
+            metadata: metadataMock,
+            startTime: new Date("2024-10-18T07:00:00"),
+            endTime: new Date("2024-10-18T08:00:00"),
+            who: "Everyone",
+            numAttendees: 100,
+            what: {value: "DT + Prayer", hyperlink: null},
+            where: {value: "Viewridge", hyperlink: null},
+            inCharge: {value: "Person1", hyperlink: null},
+            helpers: {value: "Setup: Helper1, Helper2, Helper3", hyperlink: null},
+            foodLead: {value: "", hyperlink: null},
+            childcare: {value: "", hyperlink: null},
+            notes: {value: "", hyperlink: null}
+        };
 
-    const retrievedId: string = getId(row);
+        const retrievedId: string = getId(row);
 
-    expect(retrievedId).toBe("0419f086-4f19-4ade-ac64-2edafefef23d");
-  });
+        expect(retrievedId).toBe("0419f086-4f19-4ade-ac64-2edafefef23d");
+    });
 
-  test("Row id is null", () => {
-    const getValueMock = jest.fn(() => null);
-    const metadataMock: Metadata = {
-      getId: jest.fn(),
-      getKey: jest.fn(),
-      getLocation: jest.fn(),
-      getValue: getValueMock,
-      getVisibility: jest.fn(),
-      moveToColumn: jest.fn().mockReturnThis(),
-      moveToRow: jest.fn().mockReturnThis(),
-      moveToSheet: jest.fn().mockReturnThis(),
-      moveToSpreadsheet: jest.fn().mockReturnThis(),
-      remove: jest.fn(),
-      setKey: jest.fn().mockReturnThis(),
-      setValue: jest.fn().mockReturnThis(),
-      setVisibility: jest.fn().mockReturnThis(),
-    };
-    const row: Row = {
-      metadata: metadataMock,
-      startTime: new Date("2024-10-18T07:00:00"),
-      endTime: new Date("2024-10-18T08:00:00"),
-      who: "Everyone",
-      numAttendees: 100,
-      what: {value: "DT + Prayer", hyperlink: null},
-      where: {value: "Viewridge", hyperlink: null},
-      inCharge: {value: "Person1", hyperlink: null},
-      helpers: {value: "Setup: Helper1, Helper2, Helper3", hyperlink: null},
-      foodLead: {value: "", hyperlink: null},
-      childcare: {value: "", hyperlink: null},
-      notes: {value: "", hyperlink: null}
-    };
+    test("Row id is null", () => {
+        const getValueMock = jest.fn(() => null);
+        const metadataMock: Metadata = {
+            getId: jest.fn(),
+            getKey: jest.fn(),
+            getLocation: jest.fn(),
+            getValue: getValueMock,
+            getVisibility: jest.fn(),
+            moveToColumn: jest.fn().mockReturnThis(),
+            moveToRow: jest.fn().mockReturnThis(),
+            moveToSheet: jest.fn().mockReturnThis(),
+            moveToSpreadsheet: jest.fn().mockReturnThis(),
+            remove: jest.fn(),
+            setKey: jest.fn().mockReturnThis(),
+            setValue: jest.fn().mockReturnThis(),
+            setVisibility: jest.fn().mockReturnThis(),
+        };
+        const row: Row = {
+            metadata: metadataMock,
+            startTime: new Date("2024-10-18T07:00:00"),
+            endTime: new Date("2024-10-18T08:00:00"),
+            who: "Everyone",
+            numAttendees: 100,
+            what: {value: "DT + Prayer", hyperlink: null},
+            where: {value: "Viewridge", hyperlink: null},
+            inCharge: {value: "Person1", hyperlink: null},
+            helpers: {value: "Setup: Helper1, Helper2, Helper3", hyperlink: null},
+            foodLead: {value: "", hyperlink: null},
+            childcare: {value: "", hyperlink: null},
+            notes: {value: "", hyperlink: null}
+        };
 
-    expect(() => getId(row)).toThrow(RowMissingIdError);
-  });
+        expect(() => getId(row)).toThrow(RowMissingIdError);
+    });
 
   test("Row id is an empty string", () => {
-    const getValueMock = jest.fn(() => "");
-    const metadataMock: Metadata = {
-      getId: jest.fn(),
-      getKey: jest.fn(),
-      getLocation: jest.fn(),
-      getValue: getValueMock,
-      getVisibility: jest.fn(),
-      moveToColumn: jest.fn().mockReturnThis(),
-      moveToRow: jest.fn().mockReturnThis(),
-      moveToSheet: jest.fn().mockReturnThis(),
-      moveToSpreadsheet: jest.fn().mockReturnThis(),
-      remove: jest.fn(),
-      setKey: jest.fn().mockReturnThis(),
-      setValue: jest.fn().mockReturnThis(),
-      setVisibility: jest.fn().mockReturnThis(),
-    };
-    const row: Row = {
-      metadata: metadataMock,
-      startTime: new Date("2024-10-18T07:00:00"),
-      endTime: new Date("2024-10-18T08:00:00"),
-      who: "Everyone",
-      numAttendees: 100,
-      what: {value: "DT + Prayer", hyperlink: null},
-      where: {value: "Viewridge", hyperlink: null},
-      inCharge: {value: "Person1", hyperlink: null},
-      helpers: {value: "Setup: Helper1, Helper2, Helper3", hyperlink: null},
-      foodLead: {value: "", hyperlink: null},
-      childcare: {value: "", hyperlink: null},
-      notes: {value: "", hyperlink: null}
-    };
+        const getValueMock = jest.fn(() => "");
+        const metadataMock: Metadata = {
+            getId: jest.fn(),
+            getKey: jest.fn(),
+            getLocation: jest.fn(),
+            getValue: getValueMock,
+            getVisibility: jest.fn(),
+            moveToColumn: jest.fn().mockReturnThis(),
+            moveToRow: jest.fn().mockReturnThis(),
+            moveToSheet: jest.fn().mockReturnThis(),
+            moveToSpreadsheet: jest.fn().mockReturnThis(),
+            remove: jest.fn(),
+            setKey: jest.fn().mockReturnThis(),
+            setValue: jest.fn().mockReturnThis(),
+            setVisibility: jest.fn().mockReturnThis(),
+        };
+        const row: Row = {
+            metadata: metadataMock,
+            startTime: new Date("2024-10-18T07:00:00"),
+            endTime: new Date("2024-10-18T08:00:00"),
+            who: "Everyone",
+            numAttendees: 100,
+            what: {value: "DT + Prayer", hyperlink: null},
+            where: {value: "Viewridge", hyperlink: null},
+            inCharge: {value: "Person1", hyperlink: null},
+            helpers: {value: "Setup: Helper1, Helper2, Helper3", hyperlink: null},
+            foodLead: {value: "", hyperlink: null},
+            childcare: {value: "", hyperlink: null},
+            notes: {value: "", hyperlink: null}
+        };
 
-    expect(() => getId(row)).toThrow(RowMissingIdError);
-  });
+        expect(() => getId(row)).toThrow(RowMissingIdError);
+    });
 });
