97c1092cb884f12c8ae58e009d3af810baaa7cf0
Josh Wong <jbwong05@gmail.com>
2024-10-28 23:49:03 -0700

    Add tests for getMetadata()

--- __test__/row.test.ts
+++ __test__/row.test.ts
@@ -3,9 +3,50 @@
 import { PropertiesService } from 'gasmask';
 global.PropertiesService = PropertiesService;
 
-import { getId } from "../src/main/row";
+import { getId, getMetadata } from "../src/main/row";
 import { RowMissingIdError } from '../src/main/error/rowMissingIdError';
-import { getRandomlyGeneratedMetadata, getRandomlyGeneratedRow } from './testUtils';
+import { getRandomlyGeneratedMetadata, getRandomlyGeneratedRange, getRandomlyGeneratedRow } from './testUtils';
+
+describe("getMetadata", () => {
+    it("should return the Metadata object when a single Metadata object is present", () => {
+        const metadataMock: Metadata = getRandomlyGeneratedMetadata();
+        const rangeMock: Range = getRandomlyGeneratedRange();
+        rangeMock.getDeveloperMetadata = jest.fn(() => [metadataMock]);
+
+        const retrievedMetadata: Metadata = getMetadata(rangeMock);
+
+        expect(retrievedMetadata).toBe(metadataMock);
+    });
+
+    it("should return the first Metadata object when multiple Metadata objects are present", () => {
+        const firstMetadataMock: Metadata = getRandomlyGeneratedMetadata();
+        const secondMetadataMock: Metadata = getRandomlyGeneratedMetadata();
+        const thirdMetadataMock: Metadata = getRandomlyGeneratedMetadata();
+        const rangeMock: Range = getRandomlyGeneratedRange();
+        rangeMock.getDeveloperMetadata = jest.fn(() => [firstMetadataMock, secondMetadataMock, thirdMetadataMock]);
+
+        const retrievedMetadata: Metadata = getMetadata(rangeMock);
+
+        expect(retrievedMetadata).toBe(firstMetadataMock);
+    });
+
+    it("should generate and return a Metadata object when Metadata is not present", () => {
+        const metadataMock: Metadata = getRandomlyGeneratedMetadata();
+        const rangeMock: Range = getRandomlyGeneratedRange();
+        const addDeveloperMetadataMock: jest.Mock = jest.fn();
+        rangeMock.addDeveloperMetadata = addDeveloperMetadataMock;
+        // First call should not return a Metadata object and the second one should
+        rangeMock.getDeveloperMetadata = jest.fn()
+            .mockImplementationOnce(() => [])
+            .mockImplementationOnce(() => [metadataMock]);
+
+        const retrievedMetadata: Metadata = getMetadata(rangeMock);
+
+        // Assert Metadata object was generaged with addDeveloperMetadata and that it was returned
+        expect(addDeveloperMetadataMock).toHaveBeenCalledWith("rowId");
+        expect(retrievedMetadata).toBe(metadataMock);
+    });
+});
 
 describe("getId", () => {
     it("should return id when id is present", () => {
@@ -36,4 +77,52 @@ describe("getId", () => {
 
         expect(() => getId(row)).toThrow(RowMissingIdError);
     });
+});
+
+describe("generateIdForRow", () => {
+
+});
+
+describe("hasId", () => {
+
+});
+
+describe("saveRow", () => {
+
+});
+
+describe("hasBeenSaved", () => {
+
+});
+
+describe("hasChanged", () => {
+
+});
+
+describe("getRowBasecampMapping", () => {
+
+});
+
+describe("toString", () => {
+
+});
+
+describe("getBasecampTodoRequestsForRow", () => {
+
+});
+
+describe("getBasecampTodosForLeads", () => {
+
+});
+
+describe("getBasecampTodosForHelpers", () => {
+
+});
+
+describe("clearAllRowMetadata", () => {
+
+});
+
+describe("getRoleTotoIdMap", () => {
+
 });
