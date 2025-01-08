092ab6cc34c8e891cd5f1065b78bee2c893ca86f
Josh Wong <jbwong05@gmail.com>
2025-01-07 23:12:28 -0800

    Restored environment variable conditional imports

--- config/environmentVariables.ts
+++ config/environmentVariables.ts
@@ -6,20 +6,22 @@ let BASECAMP_SCHEDULE_ID: string = "";
 // Need to use dynamic imports here so webpack does not include and leak secrets for the other environment
 // Webpack will also optimize out sections of code that do not correspond to the current environment
 // The webpack mode needs to be set to eager inline so the dynamic imports are included in the same bundle
-(async () => {
-    if (process.env.ENV === 'production') {
-        const prodModule = await import(/* webpackMode: "eager" */ './production');
-        BASECAMP_CLIENT_ID = prodModule.PROD_BASECAMP_CLIENT_ID;
-        BASECAMP_CLIENT_SECRET = prodModule.PROD_BASECAMP_CLIENT_SECRET;
-        BASECAMP_PROJECT_ID = prodModule.PROD_BASECAMP_PROJECT_ID;
-        BASECAMP_SCHEDULE_ID = prodModule.PROD_BASECAMP_SCHEDULE_ID;
-    } else {
-        const devModule = await import(/* webpackMode: "eager" */ './development');
-        BASECAMP_CLIENT_ID = devModule.DEV_BASECAMP_CLIENT_ID;
-        BASECAMP_CLIENT_SECRET = devModule.DEV_BASECAMP_CLIENT_SECRET;
-        BASECAMP_PROJECT_ID = devModule.DEV_BASECAMP_PROJECT_ID;
-        BASECAMP_SCHEDULE_ID = devModule.DEV_BASECAMP_SCHEDULE_ID;
-    }
-})();
+if(process.env.ENV === 'production') {
+    import(/* webpackMode: "eager" */ './production')
+        .then((prodModule) => {
+            BASECAMP_CLIENT_ID = prodModule.PROD_BASECAMP_CLIENT_ID;
+            BASECAMP_CLIENT_SECRET = prodModule.PROD_BASECAMP_CLIENT_SECRET;
+            BASECAMP_PROJECT_ID = prodModule.PROD_BASECAMP_PROJECT_ID;
+            BASECAMP_SCHEDULE_ID = prodModule.PROD_BASECAMP_SCHEDULE_ID;
+        })
+} else {
+    import(/* webpackMode: "eager" */ './development')
+        .then((devModule) => {
+            BASECAMP_CLIENT_ID = devModule.DEV_BASECAMP_CLIENT_ID;
+            BASECAMP_CLIENT_SECRET = devModule.DEV_BASECAMP_CLIENT_SECRET;
+            BASECAMP_PROJECT_ID = devModule.DEV_BASECAMP_PROJECT_ID;
+            BASECAMP_SCHEDULE_ID = devModule.DEV_BASECAMP_SCHEDULE_ID;
+        })
+}
 
 export { BASECAMP_CLIENT_ID, BASECAMP_CLIENT_SECRET, BASECAMP_PROJECT_ID, BASECAMP_SCHEDULE_ID };
