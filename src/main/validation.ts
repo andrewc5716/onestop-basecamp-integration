150fb77bf54442272477e8871cf7b635190a4aa9
Jack Zhang <7369089+jkzang@users.noreply.github.com>
2025-02-01 13:22:12 -0800

    Refactored the isHelperCellValid method to return a helpful and descr… (#40)

--- src/main/validation.ts
+++ src/main/validation.ts
@@ -9,15 +9,72 @@ const COLON_DELIM: string = ":";
 const COMMA_DELIMITER: string = ",";
 const STAFF_REGEX: RegExp = /\bstaff\b/gi;
 
-export function isHelperCellValid(helperCellText: string): boolean {
-    const helperLines: string[] = helperCellText.split(NEW_LINE_DELIM);
-    return helperLines.every(isHelperLineValid);
+export function validateLeadCellText(leadCellText: string): string {
+    const helperLines: string[] = leadCellText.split(NEW_LINE_DELIM);
+
+    let invalidLeadTokens: string[] = [];
+
+    helperLines.forEach((helperLine) => {
+        invalidLeadTokens = [...invalidLeadTokens, ...getInvalidLeadTokens(helperLine)]
+    });
+
+    if(invalidLeadTokens.length > 0) {
+        return generateValidationMessage(invalidLeadTokens);
+
+    } else {
+        return "";
+    }
 }
 
-function isHelperLineValid(helperLine: string): boolean {
+export function validateHelperCellText(helperCellText: string): string {
+    const helperLines: string[] = helperCellText.split(NEW_LINE_DELIM);
+
+    let invalidHelperTokens: string[] = [];
+
+    helperLines.forEach((helperLine) => {
+        invalidHelperTokens = [...invalidHelperTokens, ...getInvalidHelperTokens(helperLine)]
+    });
+
+    if(invalidHelperTokens.length > 0) {
+        return generateValidationMessage(invalidHelperTokens);
+
+    } else {
+        return "";
+    }
+}
+
+function generateValidationMessage(invalidHelperTokens: string[]): string {
+    return `Invalid identifier(s): ${invalidHelperTokens.join(', ')}`
+}
+
+function getInvalidLeadTokens(leadLine: string): string[] {
+    const leads: string[] = leadLine.split(COMMA_DELIMITER);
+
+    const invalidLeadTokens: string[] = [];
+
+    leads.forEach((leadToken) => {
+        const trimmedLeadToken = leadToken.trim()
+        if(!isLeadTokenValid(trimmedLeadToken)) {
+            invalidLeadTokens.push(trimmedLeadToken);
+        }
+    })
+
+    return invalidLeadTokens;
+}
+
+function getInvalidHelperTokens(helperLine: string): string[] {
     const helperList: string = removeRoleTextFromHelperLine(helperLine);
     const helpers: string[] = helperList.split(COMMA_DELIMITER);
-    return helpers.every(isHelperTokenValid);
+
+    const invalidHelperTokens: string[] = [];
+
+    helpers.forEach((helperToken) => {
+        if(!isHelperTokenValid(helperToken)) {
+            invalidHelperTokens.push(helperToken.trim());
+        }
+    })
+
+    return invalidHelperTokens;
 }
 
 function removeRoleTextFromHelperLine(helperLine: string): string {
@@ -45,6 +102,19 @@ function isHelperTokenValid(helperTokenWithFilters: string): boolean {
     return false;
 }
 
+function isLeadTokenValid(leadToken: string): boolean {
+    const normalizedLeadToken: string = normalizePersonName(leadToken);
+    if(normalizedLeadToken === "") {
+        return true;
+    } else if(ALIASES_MAP.hasOwnProperty(normalizedLeadToken)) {
+        return true;
+    } else if(MEMBER_MAP.hasOwnProperty(normalizePersonName(normalizedLeadToken))) {
+        return true;
+    }
+    return false;
+}
+
 function normalizeHelper(helper: string): string {
-    return helper.replace(STAFF_REGEX, '').toLowerCase().trim();
+    const normalizedHelper: string = normalizePersonName(helper);
+    return normalizedHelper.replace(STAFF_REGEX, '').toLowerCase().trim();
 }
