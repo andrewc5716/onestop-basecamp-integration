8fccd6eb8a2eaa8f94aed331ea50b62aaa20f002
achan8 <andrew_chan2@intuit.com>
2025-02-01 15:21:01 -0800

    add todolist id to env variables

--- config/production.ts
+++ config/production.ts
@@ -4,3 +4,4 @@ export const PROD_BASECAMP_CLIENT_ID: string = getScriptProperty("BASECAMP_CLIEN
 export const PROD_BASECAMP_CLIENT_SECRET: string = getScriptProperty("BASECAMP_CLIENT_SECRET") ?? "";
 export const PROD_BASECAMP_PROJECT_ID: string = getScriptProperty("BASECAMP_PROJECT_ID") ?? "";
 export const PROD_BASECAMP_SCHEDULE_ID: string = getScriptProperty("BASECAMP_SCHEDULE_ID") ?? "";
+export const PROD_BASECAMP_TODOLIST_ID: string = getScriptProperty("BASECAMP_TODOLIST_ID") ?? "";
