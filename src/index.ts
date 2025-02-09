dfd41a15563ef764647209cd4d3804bc86c374af
Andrew Chan <achan8@andrews-mbp.local>
2025-02-08 17:15:07 -0800

    limit available functions to run from console

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,13 @@
 import { onOpen } from './main/menu';
 
-export * from './main/aliases';
-export * from './main/basecamp';
-export * from './main/filter';
-export * from './main/groups';
-export * from './main/main';
-export * from './main/members';
-export * from './main/people';
-export * from './main/propertiesService';
-export * from './main/row';
-export * from './main/scan';
-export * from './main/todos';
-export * from './main/schedule';
-export * from './main/utils';
-export * from './main/validation';
+export {importOnestopToBasecamp} from './main/main';
+export {populatePeopleInDb} from './main/people';
+export {loadMembersAndGroupsFromOnestopIntoScriptProperties} from './main/utils';
+export {validateLeadCellText, validateHelperCellText} from './main/validation';
 
 // Re-export all the exports from `menu` module into Webpack `globalThis` scope, so they are available at runtime
 // for the Google Apps Script to call.
-export * from './main/menu';
+export {onOpen} from './main/menu';
 
 // Assign `global` so gas-webpack-plugin will generate a stub for `onOpen` in the top-level scope. A top-level
 // declaration is needed for Google Apps Script to detect the function, so it is available as a trigger.
